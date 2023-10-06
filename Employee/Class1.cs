505d243f13f67294dbbb199bb2f8c93c0fc7ee55
ny19vizia <ny19vizia@201-01>
2023-10-06 10:44:14 +0200

    Add project files.

---
+++ Employee/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class Dolgozok
+    {
+        
+        public string name { get; set; }
+        public int age { get; set; }
+        public string city { get; set; }
+        public string position { get; set; }
+        public string gender { get; set; }
+        public string martialStatus { get; set; }
+        public double salaryEUR { get; set; }
+    
+        public Dolgozok(string sor)
+        {
+            string[] atmeneti = sor.Split(';');
+            name = atmeneti[0];
+            age = Convert.ToInt32(atmeneti[1]);
+            city = atmeneti[2];
+            position = atmeneti[3];
+            gender = atmeneti[4];
+            martialStatus = atmeneti[5];
+            salaryEUR = Convert.ToInt32(atmeneti[6]);
+
+        }
+        public void Kiir()
+        {
+            Console.WriteLine($"Név: {name}, Kor: {age}, Város: {city}, Pozíció: {position}, Nem: {gender}, Kapcsolata: {martialStatus}, Fizetés: {salaryEUR} ");
+        }
+        
+    }
+}
---
+++ Employee/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Dolgozok> dolgozo = new List<Dolgozok>();
+            
+            using var sr = new StreamReader(
+            @"../../../src/Employees.txt",
+            Encoding.UTF8);
+            
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+        public double KorAtlag()
+        {
+            double atlag = 0;
+
+
+            return atlag;
+        }
+    }
+}
